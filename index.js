a548e9b6ea63c4625130bfb747670dcb26cc972b
Ovissss <ovis@DESKTOP-7VIVS9E.localdomain>
2022-01-23 14:01:47 +0000

    ReactDom not working

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-ReactDom.render (<h1>Hello Everyone!!! </h1>, document.getElementById("root"))
+ReactDom.render(<h1>Hello Everyone!!! </h1>, document.getElementById("root"))
+
+ReactDom.render(<ul><li>Thing 1</li><li>Thing 2</li></ul>,
+    document,getElementById("root"))
